04c1852c4e38163423651bf87558417fa0607130
Said Nurullin <s.nurullin@innopolis.university>
2024-12-05 19:42:46 +0300

    service updated

--- screaming_service.py
+++ screaming_service.py
@@ -1,16 +1,15 @@
 import pika
-import time
-
+import json
 
 def callback(ch, method, properties, body):
-    message = body.decode().upper()
+    message = json.loads(body.decode())
+    print(f'Message: {message}')
+    message = json.dumps({ 'username': message['username'].upper(), 'message': message['message'].upper(), 'timestamp': message['timestamp'] })
     channel.basic_publish(exchange='', routing_key='email_queue', body=message)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
 if __name__ == "__main__":
-    time.sleep(20)
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='uppercase_queue')
     channel.queue_declare(queue='email_queue')
